8094a0ce81b9d6ef7998cb2f3cad1e7307fa2ea2
Filip Dabrowski <filip.a.dabrowski@gmail.com>
2026-05-05 16:09:05 +0200

    Add variables file and updated main.tf
    resources defined in main.tf:
    - resource group
    - vnet
    - subnet
    - public IP
    - nsg / firewall

--- terraform/main.tf
+++ terraform/main.tf
@@ -15,7 +15,64 @@ provider "azurerm" {
 }
 
 # define the resource group
-resource "azurerm_resource_group" "immich-deployment" {
-  name     = "immich-prod"
-  location = "polandcentral"
+resource "azurerm_resource_group" "immich_rg" {
+  name     = var.rg_name
+  location = var.location
+}
+
+# create a virtual network
+resource "azurerm_virtual_network" "immich_vnet" {
+  name                = "immich-network"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.immich_rg.location
+  resource_group_name = azurerm_resource_group.immich_rg.name
+}
+
+# create a subnet
+resource "azurerm_subnet" "immich_subnet" {
+  name                 = "internal"
+  resource_group_name  = azurerm_resource_group.immich_rg.name
+  virtual_network_name = azurerm_virtual_network.immich_vnet.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
+
+# public IP
+resource "azurerm_public_ip" "immich_pip" {
+  name                = "immich-public-ip"
+  resource_group_name = azurerm_resource_group.immich_rg.name
+  location            = azurerm_resource_group.immich_rg.location
+  allocation_method   = "Dynamic"
+}
+
+# create a firewall
+resource "azurerm_network_security_group" "immich_nsg" {
+  name                = "immich-nsg"
+  location            = azurerm_resource_group.immich_rg.location
+  resource_group_name = azurerm_resource_group.immich_rg.name
+
+  # allow ssh for admin connection
+  security_rule {
+    name                       = "SSH"
+    priority                   = 1001
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+  # allow 2283 tcp port for access to the app (default immich port)
+  security_rule {
+    name                       = "Immich"
+    priority                   = 1002
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "2283"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
 }
---
+++ terraform/variables.tf
@@ -0,0 +1,9 @@
+variable "location" {
+  description = "Our Azure Region"
+  default     = "polandcentral"
+}
+
+variable "rg_name" {
+  description = "Resource group name"
+  default     = "immich-prod"
+}
