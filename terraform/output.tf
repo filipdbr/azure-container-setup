f17e68847b67a649b2c88802d4c8ce82a2c6c285
Filip Dabrowski <filip.a.dabrowski@gmail.com>
2026-05-05 17:08:38 +0200

    feature: add output.tf for easy access to IP and URL

---
+++ terraform/output.tf
@@ -0,0 +1,11 @@
+# file defines the values that will be printed after terraform apply
+
+output "public_ip_address" {
+  description = "The public IP address of the Immich server"
+  value       = azurerm_linux_virtual_machine.immich_vm.public_ip_address
+}
+
+output "immich_url" {
+  description = "The URL to access the Immich web interface"
+  value       = "http://${azurerm_linux_virtual_machine.immich_vm.public_ip_address}:${var.immich_port}"
+}
