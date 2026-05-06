35a518174db3712fb3052b7c534b6685199ebff0
Filip Dabrowski <filip.a.dabrowski@gmail.com>
2026-05-06 11:33:42 +0200

    fix: no sudo needed in the script as it will run as root

--- scripts/provision.sh
+++ scripts/provision.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
 
-sudo apt update && sudo apt upgrade -y
+export DEBIAN_FRONTEND=noninteractive
+
+ apt update &&  apt upgrade -y
 
 # official docker installation script (available at https://docs.docker.com/engine/install/ubuntu/)
 # Add Docker's official GPG key:
-sudo apt update
-sudo apt install -y ca-certificates curl
-sudo install -m 0755 -d /etc/apt/keyrings
-sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
-sudo chmod a+r /etc/apt/keyrings/docker.asc
+ apt update
+ apt install -y ca-certificates curl
+ install -m 0755 -d /etc/apt/keyrings
+ curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+ chmod a+r /etc/apt/keyrings/docker.asc
 
 # Add the repository to Apt sources:
-sudo tee /etc/apt/sources.list.d/docker.sources <<EOF
+ tee /etc/apt/sources.list.d/docker.sources <<EOF
 Types: deb
 URIs: https://download.docker.com/linux/ubuntu
 Suites: $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}")
@@ -20,16 +22,16 @@ Architectures: $(dpkg --print-architecture)
 Signed-By: /etc/apt/keyrings/docker.asc
 EOF
 
-sudo apt update
+apt update
 
 # additional docker components
-sudo apt install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+apt install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
 # start the docker and make it start automatically with each reboot
-sudo systemctl enable --now docker
+systemctl enable --now docker
 
 # adding the user defined in Terraform to the docker grooup
-sudo usermod -aG docker immich_admin
+usermod -aG docker immich_admin
 
 # Verification
 docker_status=$(systemctl is-active docker)
