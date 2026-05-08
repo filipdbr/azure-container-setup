6c510d9b6697da1147a599e9fdada9d6fa630d6e
Filip Dabrowski <filip.a.dabrowski@gmail.com>
2026-05-08 14:49:01 +0200

    feat(scripts): implement Key Vault secret retrieval with retry loop
    - Added Azure CLI installation and Managed Identity login.
    - Created a 120-second polling mechanism to fetch the DB password, ensuring the script handles Azure IAM propagation delays gracefully.

--- scripts/provision.sh
+++ scripts/provision.sh
@@ -27,6 +27,32 @@ EOF
 
 apt update
 
+# install azure CLI in order to get access to key vault
+curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash
+
+az login --identity
+
+# try to retrieve the pass for 120 seconds
+end=$((SECONDS+120))
+start=$SECONDS
+while [ $SECONDS -lt $end ]; do
+    DB_PASS=$(az keyvault secret show --name "${pass_name}" --vault-name "${vault_name}" --query value -o tsv 2>/dev/null)
+    
+    # if DB_PASS var isn't empty break the loop
+    if [ -n "$DB_PASS" ]; then
+        echo "Successfully retrieved password from Key Vault"
+        break
+    fi
+    echo "Waiting for Key Vault permissions to propagate. Time passed: $(( SECONDS - $start))"
+    sleep 10
+done
+
+# Zabezpieczenie: Jeśli po 2 minutach hasło nadal jest puste, przerwij skrypt
+if [ -z "$DB_PASS" ]; then
+    echo "Failed to retrieve database password. Exiting."
+    exit 1
+fi
+
 # additional docker components
 apt install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
@@ -37,8 +63,6 @@ systemctl enable --now docker
 usermod -aG docker immich_admin
 
 mkdir immich-app && cd immich-app
-
-git clone 
 
 wget -O docker-compose.yml https://github.com/immich-app/immich/releases/latest/download/docker-compose.yml
 
@@ -61,12 +85,13 @@ IMMICH_VERSION=v2
 
 # Connection secret for postgres. You should change it to a random password
 # Please use only the characters `A-Za-z0-9`, without special characters or spaces
-DB_PASSWORD=postgres
+DB_PASSWORD=$DB_PASS
 
 # The values below this line do not need to be changed
 ###################################################################################
-DB_USERNAME=postgres
+DB_USERNAME=$DB_USERNAME
 DB_DATABASE_NAME=immich
 EOF
 
-docker compose up -d
+chown -R ${admin_name}:${admin_name} /home/${admin_name}/immich-app
+sudo -u ${admin_name} docker compose up -d
