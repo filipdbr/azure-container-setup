fa9549dc7ca0f12014689a8af72130a29c944fda
Filip Dabrowski <filip.a.dabrowski@gmail.com>
2026-05-09 11:04:33 +0200

    fix: the name of ACR
    the name needs to be alpha numeric and unique in the scope of the whole Azure

--- terraform/containers.tf
+++ terraform/containers.tf
@@ -1,6 +1,6 @@
 # create azure container registry to store docker image of immich
 resource "azurerm_container_registry" "immich_acr" {
-  name                = "immich-container-registry"
+  name                = "immichcontainerregistry${random_id.my_kv_id.hex}" # using the same random id as generated in security.tf file
   resource_group_name = azurerm_resource_group.immich_rg.name
   location            = azurerm_resource_group.immich_rg.location
   sku                 = "Basic"
