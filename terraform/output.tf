5395f759610a44d4b801ca031a2949ec7e9e30ab
Filip Dabrowski <filip.a.dabrowski@gmail.com>
2026-05-13 13:54:42 +0200

    feat(terraform): add sensitive output for GitHub Actions credentials

--- terraform/output.tf
+++ terraform/output.tf
@@ -13,3 +13,14 @@ output "immich_url" {
 output "keyvault_name" {
   value = azurerm_key_vault.immich_kv.name
 }
+
+# after apply we can use terrform output -json azure_cred to get a JSON needed for github actions 
+output "azure_cred" {
+  sensitive = true
+  value = {
+    clientId       = azuread_application.github_actions.client_id
+    clientSecret   = azuread_service_principal_password.github_actions_pw.value
+    subscriptionId = data.azurerm_subscription.current.subscription_id
+    tenantId       = data.azurerm_client_config.current.tenant_id
+  }
+}
