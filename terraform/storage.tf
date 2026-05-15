ab1ce8080a0a9024149316018e18231b616c6e5c
Filip Dabrowski <filip.a.dabrowski@gmail.com>
2026-05-15 20:42:24 +0200

    chore: trigger deployment

--- terraform/storage.tf
+++ terraform/storage.tf
@@ -1,6 +1,6 @@
 # file will create the file share being a persistent storage for immich
 resource "azurerm_storage_account" "immich_storage_acc" {
-  name                     = "immich-storage-account"
+  name                     = "immichstorageaccount"
   resource_group_name      = azurerm_resource_group.immich_rg.name
   location                 = azurerm_resource_group.immich_rg.location
   account_tier             = "Standard"
